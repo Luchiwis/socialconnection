7ce5bc09f6c935d92b0e6f81ed15d3cb5e0a697b
lucio <creppenderman@gmail.com>
2023-10-01 20:52:28 -0300

    started connections

--- frontendtest/script.js
+++ frontendtest/script.js
@@ -6,7 +6,9 @@ canvas.height = window.innerHeight;
 class Nodes {
   constructor() {
     this.nodes = [];
+    this.connections = [];
     this.grabbing = null;
+    this.editing = null;
     this.#createEvents();
     this.#update();
   }
@@ -20,8 +22,17 @@ class Nodes {
   }
 
   draw() {
+    this.connections.forEach((connection) => connection.draw());
+
     this.nodes.forEach((node) => node.draw());
   }
+
+  connect(node1, node2) {
+    const connection = new Connection(node1, node2);
+    this.connections.push(connection);
+    return connection
+    }  
+
   #update() {
     // remove everything and redraw
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -35,7 +46,7 @@ class Nodes {
       const x = e.offsetX;
       const y = e.offsetY;
       this.nodes.forEach((node) => {
-        if (node.checkClick(x, y)) {
+        if (node.checkMouseIn(x, y)) {
           //   nodes.removeNode(node);
         }
       });
@@ -43,29 +54,30 @@ class Nodes {
 
     // Update cursor style and move node if grabbing
     canvas.addEventListener("mousemove", (e) => {
-        const x = e.offsetX;
-        const y = e.offsetY;
-        this.nodes.forEach((node) => {
-            if (this.grabbing) {
-                this.grabbing.x = x;
-                this.grabbing.y = y;
-            }
-            else if (node.checkClick(x, y)) {
-                canvas.style.cursor = "pointer";
-            } else {
-                canvas.style.cursor = "default";
-            }
-        });
+      const x = e.offsetX;
+      const y = e.offsetY;
+      if (this.grabbing) {
+        this.grabbing.x = x;
+        this.grabbing.y = y;
+        return;
+      }
+      for (const node of this.nodes) {
+        if (node.checkMouseIn(x, y)) {
+          canvas.style.cursor = "pointer";
+          break;
+        } else {
+          canvas.style.cursor = "default";
+        }
+      }
     });
-    
 
     canvas.addEventListener("mousedown", (e) => {
       const x = e.offsetX;
       const y = e.offsetY;
       this.nodes.forEach((node) => {
-        if (node.checkClick(x, y)) {
+        if (node.checkMouseIn(x, y)) {
           canvas.style.cursor = "grabbing";
-            this.grabbing = node;
+          this.grabbing = node;
         }
       });
     });
@@ -74,20 +86,20 @@ class Nodes {
       const x = e.offsetX;
       const y = e.offsetY;
       this.nodes.forEach((node) => {
-        if (node.checkClick(x, y)) {
+        if (node.checkMouseIn(x, y)) {
           canvas.style.cursor = "pointer";
           this.grabbing = null;
         }
       });
     });
 
-    // Remove node on double click
+    // Edit on double click
     canvas.addEventListener("dblclick", (e) => {
       const x = e.offsetX;
       const y = e.offsetY;
       this.nodes.forEach((node) => {
-        if (node.checkClick(x, y)) {
-          nodes.removeNode(node);
+        if (node.checkMouseIn(x, y)) {
+          this.editing = node;
         }
       });
     });
@@ -95,11 +107,12 @@ class Nodes {
 }
 
 class Node {
-  constructor(x, y, radius, color) {
+  constructor(x, y, radius = 50, text = "", color = "lightgray") {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
+    this.text = text;
   }
 
   draw() {
@@ -107,6 +120,11 @@ class Node {
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     ctx.fillStyle = this.color;
     ctx.fill();
+
+    ctx.font = "20px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText(this.text, this.x, this.y + 5);
   }
 
   move() {
@@ -115,7 +133,7 @@ class Node {
     this.draw();
   }
 
-  checkClick(x, y) {
+  checkMouseIn(x, y) {
     const distance = Math.sqrt(
       Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2)
     );
@@ -123,13 +141,56 @@ class Node {
   }
 }
 
+class Connection {
+  constructor(node1, node2) {
+    this.node1 = node1;
+    this.node2 = node2;
+    this.draw();
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.moveTo(this.node1.x, this.node1.y);
+    ctx.lineTo(this.node2.x, this.node2.y);
+    ctx.stroke();
+  }
+}
+
 // execution
 
 nodes = new Nodes();
 
-persona1 = new Node(40, 40, 10, "blue");
-
-// nodes.addNode(new Node(10, 10, 10, "red"));
+nombres = [
+  "juan",
+  "pedro",
+  "maria",
+  "jose",
+  "luis",
+  "carlos",
+  "jorge",
+  "laura",
+  "ana",
+  "lucia",
+  "daniel",
+  "david",
+  "diego",
+  "daniela",
+];
+persona1 = new Node(40, 40, 50, "lucio", "blue");
+persona2 = new Node(200, 200, 50, "jorge", "red");
 nodes.addNode(persona1);
+nodes.addNode(persona2);
 
-nodes.draw();
+for (let i = 0; i < 10; i++) {
+  nodes.addNode(
+    new Node(
+      Math.random() * canvas.width,
+      Math.random() * canvas.height,
+      50,
+      nombres[i]
+    )
+  );
+}
+
+nodes.connect(persona2,persona1)
